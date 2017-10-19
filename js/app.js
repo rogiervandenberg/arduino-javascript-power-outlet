9433821d5c2ef78992c9f298dd5593d7efdc24c3
Rogier <rogiervandenberg@gmail.com>
2017-10-19 10:53:31 +0200

    Remove Dweetio, switch to Pubnub

--- js/app.js
+++ js/app.js
@@ -1,42 +1,58 @@
 $(document).ready(function() {
+  var pubnub = new PubNub({
+    publishKey: "pub-c-d2e96420-3c82-45a1-bf67-bb8cd6bdc016",
+    subscribeKey: "sub-c-b9f865be-bfc2-11e6-91e2-02ee2ddab7fe"
+  });
 
-  checkDweetState();
+  startPubNubListener();
+  retrieveHistory();
 
-  $('#powerSwitch').change(function() {
-    if($(this).is(":checked")) {
-          dweetio.dweet_for("arduino-javascript-power-outlet", {state:"1"}, function(err, dweet){
-          if (err) {
-            console.log("too quick!");
-            checkDweetState();
-          } else {
-            console.log(dweet.thing); // "my-thing"
-            console.log(dweet.content); // The content of the dweet
-            console.log(dweet.created); // The create date of the dweet
-          }
-        })
+  $("#powerSwitch").change(function() {
+    if ($(this).is(":checked")) {
+      publishToggle(true);
     } else {
-        dweetio.dweet_for("arduino-javascript-power-outlet", {state:"0"}, function(err, dweet){
-          if (err) {
-            console.log("too quick!");
-            checkDweetState();
-          } else {
-            console.log(dweet.thing); // "my-thing"
-            console.log(dweet.content); // The content of the dweet
-            console.log(dweet.created); // The create date of the dweet
-          }
-      })
+      publishToggle(false);
     }
   });
 
-  function checkDweetState() {
-    dweetio.get_latest_dweet_for("arduino-javascript-power-outlet", function(err, dweet){
-      var dweet = dweet[0]; // Dweet is always an array of 1
-      if(dweet.content["state"] == 1) {
-          $('#powerSwitch').prop('checked', true);
-      } else {
-        $('#powerSwitch').prop('checked', false);
-      }
+  function publishToggle(on) {
+    var publishConfig = {
+      channel: "state",
+      message: on === true ? "1" : "0"
+    };
+    pubnub.publish(publishConfig, function(status, response) {
+      console.log(status, response);
     });
   }
 
+  function startPubNubListener() {
+    pubnub.addListener({
+      message: function(message) {
+        console.log("New Message!!", message);
+        setPowerSwitch(message.message == "1");
+      }
+    });
+    console.log("Subscribing..");
+    pubnub.subscribe({
+      channels: ["state"]
+    });
+  }
+
+  function retrieveHistory() {
+    pubnub.history(
+      {
+          channel: 'state',
+          count: 1, // how many items to fetch
+      },
+      function (status, response) {
+        console.log("History", response);
+        setPowerSwitch(response.messages[0].entry == "1");
+          // handle status, response
+      }
+  );
+  }
+
+  function setPowerSwitch(on) {
+    $("#powerSwitch").prop("checked", on);
+  }
 });
